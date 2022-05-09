07cfafd3105d717755c77397971ae27c8d574916
Ramal Məmmədli <memmedliramal00@gmail.com>
2022-05-09 18:01:30 +0400

    bug fix

--- Brain Boxai/assets/scripts/sustainability.js
+++ Brain Boxai/assets/scripts/sustainability.js
@@ -18,14 +18,14 @@ let button_onclick1 = document.getElementsByClassName("sus-button-onclick1");
 let toggle_item_inner = document.getElementsByClassName("sus-toggle-item-inner");
 
 for (let i = 0; i < toggle_item_inner.length; i++) {
-    button_onclick[i].addEventListener("click", () => {
-        if (toggle_item_inner[i].style.display == "none") {
-            toggle_item_inner[i].style.display = "block";
-        }
-        else if (toggle_item_inner[i].style.display = "block") {
-            toggle_item_inner[i].style.display = "none";
-        }
+  button_onclick[i].addEventListener("click", () => {
+    if (toggle_item_inner[i].style.display == "none") {
+      toggle_item_inner[i].style.display = "block";
+    }
+    else if (toggle_item_inner[i].style.display = "block") {
+      toggle_item_inner[i].style.display = "none";
+    }
 
 
-    })
+  })
 }

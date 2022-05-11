185820b6d9e929dd978398713d94637946866575
HSYNCodeAcademy <99787133+HSYNCodeAcademy@users.noreply.github.com>
2022-05-11 11:08:12 +0400

    new js

--- Brain Boxai/assets/scripts/careers.js
+++ Brain Boxai/assets/scripts/careers.js
@@ -11,3 +11,21 @@ menu_inner_menu_icon.addEventListener("click", () => {
   menu_inner_container.classList.toggle("menu-inner-container-on");
   menu_icon.classList.toggle("active");
 });
+
+let card_button = document.getElementsByClassName("card-button");
+let card_inner = document.getElementsByClassName("card-inner");
+
+for (let i = 0; card_button.length; i++) {
+    card_inner[i].style.display = "none";
+    card_button[i].innerText = "+";
+    card_button[i].addEventListener("click", () => {
+        if (card_inner[i].style.display == "none") {
+            card_inner[i].style.display = "block";
+            card_button[i].innerText = "-";
+        }
+        else if(card_inner[i].style.display == "block"){
+            card_inner[i].style.display = "none";
+            card_button[i].innerText = "+";
+        }
+    })
+}
